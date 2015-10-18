7384225e33e10209ca31a6ed795eff0d15a8fe49
Zachary Waldron <wewingames222@gmail.com>
2015-10-17 21:18:56 -0700

    Updated Page Headers

--- includes/headers.php
+++ includes/headers.php
@@ -1,12 +1,31 @@
 <?php
+public function genPath($dirdown) {
+	$tempstr = '';
+	for (int $i = 0; $i < $dirdown; $i++) {
+		$tempstr .= "../"
+	}
+	
+	return $tempstr;
+}
+
 function createHeader($foldersDown=0, $title) {
-	echo "\t<link rel='stylesheet' href='./styles/w3.css' />\n";
-	echo "\t<link rel='stylesheet' href='./styles/base.css' />\n"; 
-	echo "\t<title>$title</title>\n";
-	echo "\t<meta charset='UTF-8' />\n";
-	echo "\t<meta name='content-type' content='text/html' />\n";
-	echo "\t<meta name='author' content='Thunderbots Robotics' />\n";
-	echo "\t<meta name='viewport' content='width=device-width, initial-scale=1' />\n";
-	echo "\t<script src='http://code.jquery.com/jquery-1.11.3.min.js'></script>\n";
-	echo "\t<script src='http://www.thunderbots.x10host.com/script/api.js'></script>\n";
+	$insertedText = $foldersDown != 0 ? genPath($foldersDown) : '';
+	echo "\t\t<title>Thunderbots 5604</title>\n\
+	\t\t<meta charset='UTF-8'>\n\
+	\t\t<meta name='keywords' content='Thunderbots, Thunderbots 5604, Mountain View High School, MVHS'>\n\
+	\t\t<meta name='description' content='Thunderbots 5604 Website'>\n\
+	\t\t<meta name='author' content='Javier Anton, Zachary Waldron'>\n\
+	\t\t<meta http-equiv='X-UA-Compatible' content='IE=edge, chrome=1'>\n\
+	\t\t<link rel='icon' type='image/png' href='{$insertedText}css/img/thunderbots_icon-32x32.png' sizes='32x32' />\n\
+	\t\t<link rel='icon' type='image/png' href='{$insertedText}css/img/thunderbots_icon-16x16.png' sizes='16x16' />\n\
+	\t\t<link type='text/css' rel='stylesheet' href='{$insertedText}css/normalize.css'>\n\
+\t\t<link type='text/css' rel='stylesheet' href='{$insertedText}css/shift.css'>\n\
+	\t\t<link type='text/css' rel='stylesheet' href='{$insertedText}css/bootstrap.css'>\n\
+	\t\t<link type='text/css' rel='stylesheet' href='{$insertedText}css/main.css'>\n\
+	\t\t<link type='text/css' rel='stylesheet' href='{$insertedText}fonts\font-awesome\css\font-awesome.min.css'>\n\
+	\t\t<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>\n\
+	\t\t<!--[if lt IE 9]>\n\
+	\t\t\t<script src='http://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>\n\
+	\t\t\t<script src='http://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>\n\
+	\t\t<![endif]-->\n\"
 }
